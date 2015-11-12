a4778f54d6308237cadbeb9b1a46e88c79507960
Rob Haswell <me@robhaswell.co.uk>
2015-11-12 17:16:29 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import codecs  # To use a consistent encoding
+
+# Get the long description from the relevant file
+with codecs.open('DESCRIPTION.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='flocker_ceph_driver',
+    version='0.1',
+    description='Ceph RBD driver for ClusterHQ/Flocker',
+    long_description=long_description,
+    author='Rob Haswell',
+    author_email='rob@clusterhq.com',
+    url='https://github.com/ClusterHQ/flocker-ceph-driver',
+    license='Apache 2.0',
+
+    keywords='backend, plugin, flocker, docker, python',
+    packages=find_packages(exclude=['test*']),
+    install_requires = ['rados'],
+    data_files=[('/etc/flocker/', ['agent.yml.example'])],
+)
